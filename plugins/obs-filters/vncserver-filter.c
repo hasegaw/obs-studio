8f1456ff6d50071813401d21cd50c97d1eb0e567
Takeshi HASEGAWA <hasegaw@gmail.com>
2016-01-24 05:11:27 +0900

    obs-filters: First functionable vncserver prototype.
    Some lines are hardcoded for 1280x720 on Mac.
    
    Signed-off-by: Takeshi HASEGAWA <hasegaw@gmail.com>

--- plugins/obs-filters/vncserver-filter.c
+++ plugins/obs-filters/vncserver-filter.c
@@ -1,8 +1,16 @@
+#include "obs.h"
+#include "obs-internal.h"
+
 #include <obs-module.h>
 #include <graphics/vec4.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <rfb/rfb.h>
+
+#include <libavutil/opt.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+
 
 #define SETTING_DISPLAY                "Display"
 
@@ -14,23 +22,35 @@
 struct vncpreview_filter_data {
 	obs_source_t                   *context;
 
+    int                             update;
+    pthread_mutex_t                 fb_mutex;
 //	gs_effect_t                    *effect;
 
 	gs_eparam_t                    *display;
 
     pthread_t                       thread;
-    pthread_mutex_t                 fb_mutex;
 
-    int                             update;
-    int                             _frame;
     char                           *fb;
+    int                             fb_linesize;
+	AVPicture          dst_picture;
     int                             destroy;
+
+	struct SwsContext  *swscale;
 };
 
 const char *vncpreview_filter_name(void *unused)
 {
 	UNUSED_PARAMETER(unused);
 	return obs_module_text("Preview VNC Service");
+}
+
+static void vncpreview_render_display(void *data, uint32_t cx, uint32_t cy)
+{
+    blog(LOG_ERROR, "CALLBACK!");
+
+	UNUSED_PARAMETER(data);
+	UNUSED_PARAMETER(cx);
+	UNUSED_PARAMETER(cy);
 }
 
 static void vncpreview_filter_update(void *data, obs_data_t *settings)
@@ -62,24 +82,107 @@ static void vncpreview_filter_destroy(void *data)
 	bfree(data);
 }
 
-int vncserver_grab_frame(struct vncpreview_filter_data *filter)
+
+#define RNDTO2(X) ( ( (X) & 0xFFFFFFFE ) )
+#define RNDTO32(X) ( ( (X) % 32 ) ? ( ( (X) + 32 ) & 0xFFFFFFE0 ) : (X) )
+
+static void vncserver_render_callback(void *param, struct video_data *frame)
+{
+    struct vncpreview_filter_data *filter = param;
+    static struct SwsContext *swscale;
+
+    filter->swscale = NULL;
+//    enum video_format format = video_output_get_format(video);
+
+#if 0
+    if (pthread_mutex_trylock(&filter->fb_mutex) != 0)
+        // Failed to lock.
+        // Probably another process is accessing to the buffer.
+        return;
+
+    if (0) //filter->update == 0)
+    {
+        pthread_mutex_unlock(&filter->fb_mutex);
+        return;
+    }
+#else
+    pthread_mutex_unlock(&filter->fb_mutex);
+#endif
+
+    struct video_t *video = obs_get_video();
+	const struct video_output_info *voi = video_output_get_info(video);
+    int width    = voi->width;
+    int height   = voi->height;
+#if 0
+    swsCtx = sws_getCachedContext ( swsCtx, width, height,
+        AV_PIX_FMT_NV12, width, height, AV_PIX_FMT_RGBA, 
+            SWS_LANCZOS | SWS_ACCURATE_RND , NULL, NULL, NULL );
+#else
+    if (filter->swscale) {
+        swscale = filter->swscale;
+    } else {
+        swscale = sws_getContext(
+            width, height, AV_PIX_FMT_NV12,
+            width, height, AV_PIX_FMT_RGBA,
+            0, NULL, NULL, NULL
+        );
+        filter->swscale = swscale;
+    }
+#endif
+    sws_scale(
+        swscale,
+        (const uint8_t *const *)frame->data,
+        (const int*)frame->linesize,
+        0, height, &filter->fb,
+        filter->dst_picture.linesize
+    );
+
+    filter->update = 1;
+
+    pthread_mutex_unlock(&filter->fb_mutex);
+}
+
+static void vncserver_render()
+{
+}
+
+static void vncserver_init_video(struct vncpreview_filter_data *filter)
+{
+    struct video_t *video = obs_get_video();
+    enum video_format format = video_output_get_format(video);
+    printf("obs_get_video() = %p\n", video);
+    printf("format = %d\n", format);
+
+	const struct video_output_info *voi = video_output_get_info(video);
+    printf("voi: format %d fps %d w %d h %d\n",
+        voi->format,
+        voi->fps_num,
+        voi->width,
+        voi->height
+    );
+
+    bool r = video_output_connect(video, NULL, vncserver_render_callback, filter);
+    if (r) {
+        printf("vncserver-filter: Success to connect\n");
+    } else {
+        printf("vncserver-filter: Failed to connect\n");
+    }
+
+}
+
+static int vncserver_grab_frame(rfbScreenInfoPtr server, struct vncpreview_filter_data *filter)
 {
     if (filter->fb == NULL)
         return 0;
 
-    if (! filter->update)
-    {
-        // printf("frame is not updated. waiting for next frame\n");
-        while (! filter->update)
-            usleep(1000);
-    }
+//    if (! filter->update)
+//        return 0;
 
     /* Update VNC frame buffer */
-    pthread_mutex_lock(&filter->fb_mutex);
-    memset(filter->fb, filter->_frame & 0xFF, (1280 * 720 * 4));
-    filter->_frame += 16;
+    //pthread_mutex_lock(&filter->fb_mutex);
+    memcpy(server->frameBuffer, filter->fb, 1280 * 720 * 4);
     filter->update = 0;
-    pthread_mutex_unlock(&filter->fb_mutex);
+    //pthread_mutex_unlock(&filter->fb_mutex);
 
     return 1;
 }
@@ -99,22 +202,30 @@ void *vncserver_worker_thread(void *param)
 
     server->desktopName = "OBS Preview VNC Service";
     server->frameBuffer=(char*)malloc(WIDTH*HEIGHT*BPP);
+
+    filter->fb = (char*)malloc(WIDTH * HEIGHT * BPP);
+    filter->fb_linesize = 1280 * 4;
+    int ret = avpicture_alloc(&filter->dst_picture, AV_PIX_FMT_BGRA, 1280, 720);
+    filter->dst_picture.linesize[0] = 1280 * 4;
+
     server->alwaysShared=(1==1);
 
-    filter->fb = server->frameBuffer;
 
     /* Initialize the server */
     rfbInitServer(server);
 
     /* Loop, processing clients and taking pictures */
     while ((!filter->destroy) && rfbIsActive(server)) {
-        if (1) //TimeToTakePicture())
-            //if (TakePicture((unsigned char *)server->frameBuffer))
-                vncserver_grab_frame(filter);
-                rfbMarkRectAsModified(server,0,0,WIDTH,HEIGHT);
+        int updated= 0;
+
+        if (1)
+        updated = vncserver_grab_frame(server, filter);
+
+        if(1)
+            rfbMarkRectAsModified(server,0,0,WIDTH,HEIGHT);
 
         //usec = server->deferUpdateTime*1000;
-        rfbProcessEvents(server,1000);
+        rfbProcessEvents(server,100000);
     }
 
     printf("vncserver worker thread stopped\n");
@@ -124,7 +235,10 @@ static void *vncpreview_filter_create(obs_data_t *settings, obs_source_t *contex
 {
 	struct vncpreview_filter_data *filter =
 		bzalloc(sizeof(struct vncpreview_filter_data));
+
+
 //	char *effect_path = obs_module_file("preview_vncserver.service");
+
 
     pthread_mutex_init(&filter->fb_mutex, NULL);
     pthread_create(&filter->thread, NULL, &vncserver_worker_thread, filter);
@@ -148,6 +262,7 @@ static void *vncpreview_filter_create(obs_data_t *settings, obs_source_t *contex
 //	}
 
 	vncpreview_filter_update(filter, settings);
+    vncserver_init_video(filter);
 	return filter;
 }
 
@@ -169,13 +284,13 @@ static void vncpreview_filter_render(void *data, gs_effect_t *effect)
         height = obs_source_get_height(filter->context);
         width = obs_source_get_width(filter->context);
 
-        printf("height %d width %d\n", height, width);
+        // printf("height %d width %d\n", height, width);
 
         obs_source_skip_video_filter(filter->context);
         filter->update = 1;
         pthread_mutex_unlock(&filter->fb_mutex);
     } else {
-        printf("vncpreview_filter_render: dropped a frame (mutex locked)\n");
+        // printf("vncpreview_filter_render: dropped a frame (mutex locked)\n");
     }
 
 	UNUSED_PARAMETER(effect);
